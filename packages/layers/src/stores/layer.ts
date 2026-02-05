f84f9035a1483dee2fd290d038266cc17aec661f
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-02-05 17:48:11 +0100

    GPS 413: removing timeslider variables and functions from the layer store

--- packages/layers/src/stores/layer.ts
+++ packages/layers/src/stores/layer.ts
@@ -10,7 +10,6 @@ export const useLayerStore = defineStore('layers', () => {
     /** Layer that's shown as background layer */
     const backgroundLayer = ref<Layer | null>()
 
-    const previewYear = ref<undefined | number>(undefined)
     const greatestZIndex = computed(() => {
         return layers.value.length
     })
@@ -23,13 +22,6 @@ export const useLayerStore = defineStore('layers', () => {
 
     const visibleLayers = computed(() => {
         return layers.value.filter((layer) => layer.isVisible)
-    })
-
-    const visibleLayersWithTimeConfig = computed(() => {
-        log.debug('visible Layers With Time Config is not yet implemented.')
-        return layers.value.filter(
-            (layer) => layer.isVisible /** && layer.hasTimeConfig <-- we'll see where it is */
-        )
     })
 
     function addLayer(layer: Layer) {
@@ -117,9 +109,6 @@ export const useLayerStore = defineStore('layers', () => {
         layer.info = info
     }
 
-    function setPreviewYear(year: number | undefined) {
-        previewYear.value = year
-    }
     /**
      * I don't think this should be exported... if this is needed outside of this store, then maybe
      * this should trigger a re-thinking of the architecture here
@@ -155,7 +144,6 @@ export const useLayerStore = defineStore('layers', () => {
         greatestZIndex,
         sortedLayers,
         visibleLayers,
-        visibleLayersWithTimeConfig,
         // action
         addLayer,
         toggleVisibility,
@@ -163,7 +151,6 @@ export const useLayerStore = defineStore('layers', () => {
         setLayerInfo,
         setDimension,
         setBackground,
-        setPreviewYear,
         removeLayer,
     }
 })
