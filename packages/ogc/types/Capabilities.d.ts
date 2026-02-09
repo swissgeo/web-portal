e32f3df336cc0ff75a57d92e65cea57a66d6c82d
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-02-09 15:58:15 +0100

    GPS-456 Expos another common type from shared

--- packages/ogc/types/Capabilities.d.ts
+++ packages/ogc/types/Capabilities.d.ts
@@ -1,3 +1,8 @@
+/**
+ * Expose some common types so that not all the packages need to install the openlayers package as
+ * dependency
+ */
+
 export type WMTSCapabilities = ReturnType<
     // eslint-disable-next-line @typescript-eslint/consistent-type-imports
     InstanceType<typeof import('ol/format/WMTSCapabilities').default>['read']
@@ -6,4 +11,9 @@ export type WMTSCapabilities = ReturnType<
 export type WMSCapabilities = ReturnType<
     // eslint-disable-next-line @typescript-eslint/consistent-type-imports
     InstanceType<typeof import('ol/format/WMTSCapabilities').default>['read']
+>
+
+export type GeoJSON = ReturnType<
+    // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+    InstanceType<typeof import('ol/format/GeoJSON').default>['read']
 >
