aa3e03d1583197db47098d3346245596c0ba972f
Ismail Sunni <imajimatika@gmail.com>
2026-02-18 16:50:47 +0700

    GPS-352: Fetch vector tile files from remote API instead of local disk

--- packages/main/server/api/v1/layers/swissgeo/vectorTilesTest.ts
+++ packages/main/server/api/v1/layers/swissgeo/vectorTilesTest.ts
@@ -1,9 +1,5 @@
-import fs from 'node:fs/promises'
-
 export default defineEventHandler(async (event) => {
-    // path is relative to the package
-    const path = `../../ogc-records/tiles.json`
-    const data = await fs.readFile(path)
+    const data = await $fetch('https://services.dev.sgdi.tech/api/oar/v0/tiles.json?lang=en')
 
     appendResponseHeader(event, 'Content-Type', 'application/json')
     appendResponseHeader(event, 'Cache-Control', `max-age=${60 * 60}`)
