0fdacb994747046e63092e7eb3d062da359b23fa
Ismail Sunni <imajimatika@gmail.com>
2026-02-18 14:02:10 +0700

    GPS-480: Wire main app to mapViewStore and @swissgeo/timeslider

--- packages/main/app/components/toolbox/toolboxButtons/TimeSliderButton.vue
+++ packages/main/app/components/toolbox/toolboxButtons/TimeSliderButton.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { LucideIcon, useUiStore } from '@swissgeo/skeleton'
+import { LucideIcon } from '@swissgeo/skeleton'
 
-const uiStore = useUiStore()
-
-function toggleTimeSlider(): void {
-    uiStore.toggleTimeSlider()
-}
+const mapViewStore = useMapViewStore()
 </script>
 
 <template>
-    <div id="timeslider-button">
+    <div>
         <button
+            id="timeSliderButton"
             class="toolbox-button d-print-none mb-1 h-[40px] w-[40px] cursor-pointer rounded-[20px] text-white"
-            :class="uiStore.isTimeSliderVisible ? 'bg-blue-600' : 'bg-black'"
+            :class="mapViewStore.isTimeSliderVisible ? 'bg-blue-600' : 'bg-black'"
             data-cy="time-slider-button"
-            @click="toggleTimeSlider()"
+            @click="mapViewStore.toggleTimeSlider()"
         >
             <LucideIcon
                 name="TimerReset"
