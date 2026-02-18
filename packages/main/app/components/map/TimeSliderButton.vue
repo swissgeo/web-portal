0937b733ce906c9185c30189f68a58bb4784572f
Ismail Sunni <imajimatika@gmail.com>
2026-02-18 15:46:06 +0700

    GPS-480: Position TimeSlider dynamically based on sidebar width

--- packages/main/app/components/map/TimeSliderButton.vue
+++ packages/main/app/components/map/TimeSliderButton.vue
@@ -2,11 +2,13 @@
 import type { Dimension, Layer } from '@swissgeo/layers'
 
 import { useLayerStore } from '@swissgeo/layers'
+import { useSidebarStore } from '@swissgeo/skeleton'
 import { TimeSlider } from '@swissgeo/timeslider'
 import type { LayerWithTime } from '@swissgeo/timeslider'
 
 const mapViewStore = useMapViewStore()
 const layerStore = useLayerStore()
+const sidebarStore = useSidebarStore()
 
 const timeLayers = computed((): LayerWithTime[] =>
     layerStore.layers.filter(
@@ -41,7 +43,8 @@ function onUpdateVisibility({ uuid, isVisible }: { uuid: string; isVisible: bool
 <template>
     <div
         v-if="mapViewStore.isTimeSliderVisible"
-        class="fixed top-4 right-24 left-[420px] z-50"
+        class="fixed top-4 right-24 z-50"
+        :style="{ left: sidebarStore.sidebarWidth + 8 + 'px' }"
     >
         <TimeSlider
             :layers="timeLayers"
