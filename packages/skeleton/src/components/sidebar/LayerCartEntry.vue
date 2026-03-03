acbdd2878a4c71877e667d5a897eed8c920b280d
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-03-03 10:19:59 +0100

    GPS-513: getting rid of lucideIcon custom buttons everywhere.

--- packages/skeleton/src/components/sidebar/LayerCartEntry.vue
+++ packages/skeleton/src/components/sidebar/LayerCartEntry.vue
@@ -88,21 +88,21 @@ function removeLayer() {
     <li>
         <div class="flex">
             <IconButton
-                :iconName="layer.isVisible ? 'Eye' : 'EyeOff'"
+                :iconName="layer.isVisible ? 'Eye' : 'Eye-Off'"
                 @click="toggleVisibility()"
                 severity="secondary"
             />
             <div class="flex flex-col justify-between">
                 <IconButton
                     :disabled="layerZIndex === layersLength - 1"
-                    iconName="ChevronUp"
+                    iconName="Chevron-Up"
                     severity="secondary"
                     class="h-0.5"
                     @click="moveUp()"
                 ></IconButton>
                 <IconButton
                     :disabled="layerZIndex === 0"
-                    iconName="ChevronDown"
+                    iconName="Chevron-Down"
                     severity="secondary"
                     class="h-0.5"
                     @click="moveDown()"
