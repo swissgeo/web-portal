1ba3cd216f139d1f79aa5da6f1d5c5b8d4e947e1
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-03-30 13:47:26 +0200

    GPS-577 Fix the type exporting of @swissgeo/map

--- packages/map/src/types/index.ts
+++ packages/map/src/types/index.ts
@@ -1,13 +1,1 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import type { DefineComponent } from 'vue'
-
-export interface ActionDispatcher {
-    name: string
-}
-
-export type PROJECTION_EPSG = Record<string, string>
-export type MapModule = DefineComponent<any, any, any>
-export type TimeSlider = DefineComponent<any, any, any>
-
 export type * from './layers'
