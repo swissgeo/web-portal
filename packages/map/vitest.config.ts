7afe5ae2346a3006cd523e77cab44c910945cf4a
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-03-31 14:13:20 +0200

    GPS-577 Add missing vitest.config.ts to package/map

---
+++ packages/map/vitest.config.ts
@@ -0,0 +1,15 @@
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        environment: 'happy-dom',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [vue()],
+})
