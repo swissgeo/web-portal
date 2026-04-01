b4b6e8e86ad5e6e19b15fa05c58154437c1b1ae2
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-04-01 15:25:42 +0200

    GPS-600: mock share functionality in context menu popup

--- packages/map/src/openlayers/OpenLayersContextMenuPopupShare.vue
+++ packages/map/src/openlayers/OpenLayersContextMenuPopupShare.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
-import { Copy } from 'lucide-vue-next'
+import { Check, Copy } from 'lucide-vue-next'
+
+import { useClipboard } from '../composables/useClipboard.composable'
 
 defineProps<{
     coordinate: [number, number] | null
 }>()
+
+const SHARE_URL = 'https://map.geo.admin.ch/?...'
+
+const { copy, copied } = useClipboard()
 </script>
 
 <template>
     <div class="flex items-center justify-between gap-4 px-4 py-3">
         <span class="text-sm text-gray-700">
-            <!-- Share URL or embed code will go here -->
-            https://map.geo.admin.ch/?...
+            {{ SHARE_URL }}
         </span>
-        <button class="shrink-0 rounded p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-700">
-            <Copy :size="14" />
+        <button
+            class="shrink-0 rounded p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-700"
+            @click="copy(SHARE_URL)"
+        >
+            <Check
+                v-if="copied"
+                :size="14"
+                class="text-green-500"
+            />
+            <Copy
+                v-else
+                :size="14"
+            />
         </button>
     </div>
 </template>
