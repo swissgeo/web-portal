a1839c1bf30a2cfa08d44a3bd0db4858136024de
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-10 10:54:48 +0200

    GPS-588 Wrap the app with UApp for toasting functionality

--- packages/main/app/app.vue
+++ packages/main/app/app.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-    <NuxtLayout>
-        <!-- this is needed so that the change of language doesn't rerender every component
+    <UApp>
+        <NuxtLayout>
+            <!-- this is needed so that the change of language doesn't rerender every component
         Otherwise we re-render all the components and all the layers are being reloaded where it's
         not necessary, resulting for instance in the loss of the current opacity
         -->
-        <NuxtPage :page-key="(route) => route.path.replace(/^\/(de|fr|en|it|rm)/, '')" />
-    </NuxtLayout>
+            <NuxtPage :page-key="(route) => route.path.replace(/^\/(de|fr|en|it|rm)/, '')" />
+        </NuxtLayout>
+    </UApp>
 </template>
