6ba371230cb33e797ac4d24c429366720194a248
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-10 17:38:00 +0200

    GPS-630 Make the log levels configurable via env

--- packages/main/app/utils/__tests__/parseWantedLogLevels.spec.ts
+++ packages/main/app/utils/__tests__/parseWantedLogLevels.spec.ts
@@ -4,40 +4,36 @@ import parseWantedLogLevels from '../parseWantedLoglevels'
 
 describe('parseWantedLogLevels', () => {
     it.each([
-        [['Error'], [0]],
-        [['Warn'], [1]],
-        [['Info'], [2]],
-        [['Debug'], [3]],
-        [
-            ['Error', 'Warn'],
-            [0, 1],
-        ],
-        [
-            ['Error', 'Warn', 'Info'],
-            [0, 1, 2],
-        ],
-        [
-            ['Error', 'Warn', 'Info', 'Debug'],
-            [0, 1, 2, 3],
-        ],
-    ] as [string[], number[]][])(
-        'parses correctly the levels',
-        (input: string[], output: number[]) => {
-            const logLevels = parseWantedLogLevels(input)
-            expect(logLevels).toEqual(output)
-        }
-    )
+        ['Error', [0]],
+        ['Warn', [1]],
+        ['Info', [2]],
+        ['Debug', [3]],
+        ['Error,Warn', [0, 1]],
+        ['Error,Warn,Info', [0, 1, 2]],
+        ['Error,Warn,Info,Debug', [0, 1, 2, 3]],
+    ] as [string, number[]][])('parses correctly the levels', (input: string, output: number[]) => {
+        const logLevels = parseWantedLogLevels(input)
+        expect(logLevels).toEqual(output)
+    })
 
     it('can deal with all cases', () => {
-        expect(parseWantedLogLevels(['ErrOr'])).toEqual([0])
-        expect(parseWantedLogLevels(['error'])).toEqual([0])
-        expect(parseWantedLogLevels(['ERROR'])).toEqual([0])
-        expect(parseWantedLogLevels(['eRROR'])).toEqual([0])
-        expect(parseWantedLogLevels(['eRROR'])).toEqual([0])
+        expect(parseWantedLogLevels('ErrOr')).toEqual([0])
+        expect(parseWantedLogLevels('error')).toEqual([0])
+        expect(parseWantedLogLevels('ERROR')).toEqual([0])
+        expect(parseWantedLogLevels('eRROR')).toEqual([0])
+        expect(parseWantedLogLevels('eRROR')).toEqual([0])
+    })
+
+    it('can deal with weird whitespacing', () => {
+        expect(parseWantedLogLevels('error ')).toEqual([0])
+        expect(parseWantedLogLevels(' error ')).toEqual([0])
+        expect(parseWantedLogLevels('     error')).toEqual([0])
+        expect(parseWantedLogLevels('error, warn')).toEqual([0, 1])
+        expect(parseWantedLogLevels(' error,    warn,debug')).toEqual([0, 1, 3])
     })
 
     it('ignores unknown log levels', () => {
-        expect(parseWantedLogLevels(['eRROR', 'unknown'])).toEqual([0])
-        expect(parseWantedLogLevels(['eRROR', 'debug', 'deb0rk'])).toEqual([0, 3])
+        expect(parseWantedLogLevels('eRROR,unknown')).toEqual([0])
+        expect(parseWantedLogLevels('eRROR,debug,deb0rk')).toEqual([0, 3])
     })
 })
--- packages/main/app/utils/parseWantedLoglevels.ts
+++ packages/main/app/utils/parseWantedLoglevels.ts
@@ -1,3 +1,5 @@
+// Allow the console here because we're only initializing the log levels at this point
+/* eslint-disable no-console */
 import { LogLevel } from '@swissgeo/log'
 
 /**
@@ -6,13 +8,15 @@ import { LogLevel } from '@swissgeo/log'
  * At some point this should probably be switched to union types, but until we have
  * ported that package to this code base, we'll leave it as that
  */
-export default function parseWantedLogLevels(wantedLogLevels: string[]) {
+export default function parseWantedLogLevels(wantedLogLevels: string) {
     // Create an enumerated array where the keys are lowercase
     const possibleLevels = Object.entries(LogLevel).map(([level, value]) => [
         level.toLowerCase(),
         value,
     ])
-    const sanitizedWantedLoglevels = wantedLogLevels.map((level) => level.toLowerCase())
+    const sanitizedWantedLoglevels = wantedLogLevels
+        .split(',')
+        .map((level) => level.toLowerCase().trim())
 
     const wantedLevels = []
     for (const level of sanitizedWantedLoglevels) {
@@ -20,10 +24,9 @@ export default function parseWantedLogLevels(wantedLogLevels: string[]) {
         if (value) {
             wantedLevels.push(value[1] as LogLevel)
         } else {
-            // Allow the console.warn here because we're only initializing the log levels at this point
-            // eslint-disable-next-line no-console
             console.warn(`Wanted log level ${level} is an invalid value`)
         }
     }
+    console.log('Setting the log levels to', wantedLevels)
     return wantedLevels
 }
