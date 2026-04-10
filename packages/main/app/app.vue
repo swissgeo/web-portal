61cb09147772f450c698c02be4a8ebcd7e61391d
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-10 17:49:47 +0200

    GPS-630 Refactor print info to plugin

--- packages/main/app/app.vue
+++ packages/main/app/app.vue
@@ -1,22 +1,4 @@
-<script lang="ts" setup>
-const config = useRuntimeConfig()
-
-const version = config.public.version
-const buildTime = config.public.buildTime
-
-onMounted(() => {
-    printInfo()
-})
-
-function printInfo() {
-    const log = (msg: string, style: string = 'color: #1f576b; font-size: 1.2rem;') =>
-        // eslint-disable-next-line no-console
-        console.log(`%c${msg}`, style)
-    log('SWISSGEO', 'color: #1f576b;; font-weight: bold; font-size: 2rem;')
-    log(`Version: ${version}`)
-    log(`Build Time: ${buildTime}`)
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
     <NuxtLayout>
