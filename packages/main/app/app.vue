120e8b13541e58c68dc1995d66dc98a6a3ebb252
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-10 12:07:28 +0200

    GPS-588 Add toaster functionality

--- packages/main/app/app.vue
+++ packages/main/app/app.vue
@@ -1,7 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const toaster = { position: 'top-right' as const }
+</script>
 
 <template>
-    <UApp>
+    <UApp :toaster="toaster">
         <NuxtLayout>
             <!-- this is needed so that the change of language doesn't rerender every component
         Otherwise we re-render all the components and all the layers are being reloaded where it's
---
+++ packages/main/app/composables/useToaster.ts
@@ -0,0 +1,45 @@
+/**
+ * Basically just a wrapper with some helping functions
+ * We could theoretically be using the toaster directly, but I suppose it's a good idea
+ * to centralize all the calls here and get a consistant usage
+ */
+export function useToaster() {
+    const { $i18n } = useNuxtApp()
+    const toast = useToast()
+
+    function showWarning(message: string, overrides?: Parameters<typeof toast.add>[0]) {
+        toast.add({
+            title: $i18n.t('toaster.warningTitle'),
+            description: message,
+            color: 'warning',
+            icon: 'i-lucide-circle-alert',
+            ...overrides,
+        })
+    }
+
+    function showError(message: string, overrides?: Parameters<typeof toast.add>[0]) {
+        toast.add({
+            title: $i18n.t('toaster.errorTitle'),
+            description: message,
+            color: 'error',
+            icon: 'i-lucide-circle-x',
+            ...overrides,
+        })
+    }
+
+    function showSuccess(message: string, overrides?: Parameters<typeof toast.add>[0]) {
+        toast.add({
+            title: $i18n.t('toaster.successTitle'),
+            description: message,
+            color: 'success',
+            icon: 'i-lucide-circle-check',
+            ...overrides,
+        })
+    }
+
+    return {
+        showWarning,
+        showError,
+        showSuccess,
+    }
+}
