9dc578875a83ab7c497d01c732559c45605f71c4
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-10 17:47:37 +0200

    GPS-630 Refactoring the log level setter to a plugin
    Decluttering the app.vue component a bit

---
+++ packages/main/app/plugins/setLogLevels.ts
@@ -0,0 +1,13 @@
+import log from '@swissgeo/log'
+import parseWantedLogLevels from '~/utils/parseWantedLoglevels'
+
+export default defineNuxtPlugin({
+    name: 'setLogLevels',
+
+    hooks: {
+        'app:created'() {
+            const config = useRuntimeConfig()
+            log.wantedLevels = parseWantedLogLevels(config.public.wantedLogLevels)
+        },
+    },
+})
