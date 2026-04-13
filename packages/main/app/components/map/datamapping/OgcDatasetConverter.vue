d8e8afe62ff15173cea0bb78687eb741029eea38
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-04-13 19:37:57 +0200

    GPS-583: map Layers handling
    - we created functions to handle the layers data without touching the source data
    - added displayname to the layers data so we can show it in the layercart without calling the source Data
    - still left to do : reordering layers change also the source layers order, and it causes re-rendering of layers.
    - still left to do : we must have a way to handle zIndex in a cleaner way now.

--- packages/main/app/components/map/datamapping/OgcDatasetConverter.vue
+++ packages/main/app/components/map/datamapping/OgcDatasetConverter.vue
@@ -72,6 +72,7 @@ const layerData = computed((): MapLayer => {
         isVisible: layer.isVisible,
         opacity: layer.opacity,
         zIndex: layerZIndex.value,
+        displayName: layer.info?.displayName ?? layer.humanId,
     }
 })
 
--- packages/main/app/components/sidebar/LayerCartEntry.vue
+++ packages/main/app/components/sidebar/LayerCartEntry.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 // TODO : map view store alterations
-import type { Layer } from '@swissgeo/layers'
+import type { Layer as MapLayer } from '@swissgeo/map'
 
 import { useDrawingStore } from '@swissgeo/drawing'
 import { useLayerStore } from '@swissgeo/layers'
@@ -8,25 +8,17 @@ import { getDisplayNameFromTimestamp } from '@swissgeo/shared'
 import { IconButton } from '@swissgeo/skeleton'
 import { computed } from 'vue'
 
-const { layer, index } = defineProps<{
-    layer: Layer
-    index: number
+const { layer, layerIndex } = defineProps<{
+    layer: MapLayer
+    layerIndex: number
 }>()
 
 const layerStore = useLayerStore()
 const drawingStore = useDrawingStore()
 const mapViewStore = useMapViewStore()
+const bgLayerModifier = computed(() => (layerStore.backgroundLayer ? 1 : 0))
 
-const layersLength = computed(() => layerStore.layers.length)
-
-const displayName = computed(() => {
-    // get the info from the dataset
-    if (layer.info && layer.info.displayName) {
-        return layer.info.displayName
-    } else {
-        return layer.humanId
-    }
-})
+const layersLength = computed(() => mapViewStore.mapLayers.length)
 
 const currentTime = computed({
     get() {
@@ -56,28 +48,32 @@ const getTimestampName = (time: string) => {
 const opacityPercent = computed({
     get: () => Math.round(layer.opacity * 100),
     set: (value: number) => {
-        layerStore.setOpacity(layer.uuid, value / 100)
+        handleOpacityChange(value / 100)
+        //layerStore.setOpacity(layerIndex, value / 100)
     },
 })
 
 function handleOpacityChange(value: number) {
-    layerStore.setOpacity(layer.uuid, value / 100)
+    mapViewStore.updateLayerOpacity(layerIndex, value / 100)
 }
 
 function toggleVisibility() {
-    layerStore.toggleVisibility(layer.uuid)
+    mapViewStore.toggleVisibility(layerIndex)
 }
 
 function moveUp() {
-    layerStore.moveLayerUp(layer.uuid)
+    mapViewStore.moveLayerUp(layerIndex)
+    layerStore.moveLayerUp(layerIndex - bgLayerModifier.value)
 }
 
 function moveDown() {
-    layerStore.moveLayerDown(layer.uuid)
+    mapViewStore.moveLayerDown(layerIndex)
+    layerStore.moveLayerDown(layerIndex - bgLayerModifier.value)
 }
 
 function removeLayer() {
-    layerStore.removeLayer(layer.uuid)
+    mapViewStore.removeLayer(layerIndex)
+    //layerStore.removeLayer(layerIndex - bgLayerModifier.value)
     if (layer.uuid === drawingStore.drawingKMLLayerUuid) {
         drawingStore.resetDrawingLayerUuid()
         drawingStore.clearDrawingFeatures()
@@ -95,14 +91,14 @@ function removeLayer() {
             />
             <div class="flex flex-col justify-between">
                 <IconButton
-                    :disabled="index === layersLength - 1"
+                    :disabled="layerIndex === layersLength - bgLayerModifier"
                     iconName="Chevron-Up"
                     severity="secondary"
                     class="h-0.5"
                     @click="moveUp()"
                 ></IconButton>
                 <IconButton
-                    :disabled="index === 0"
+                    :disabled="layerIndex === bgLayerModifier"
                     iconName="Chevron-Down"
                     severity="secondary"
                     class="h-0.5"
@@ -113,10 +109,10 @@ function removeLayer() {
         <div class="flex-1">
             <div
                 class="overflow-x-hidden text-nowrap"
-                :title="layer.humanId"
+                :title="layer.title"
                 :class="{ 'text-gray-300': !layer.isVisible }"
             >
-                {{ displayName }}
+                {{ layer.displayName }}
             </div>
             <div class="mt-2 flex items-center gap-2">
                 <span class="text-xs text-gray-600">Opacity:</span>
