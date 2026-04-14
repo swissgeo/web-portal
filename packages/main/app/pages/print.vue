95cd68c91c889ec64c7cd98a6246bed2bd4e79e2
jonathanlurie <lurie.jo@gmail.com>
2026-04-14 14:03:19 +0200

    GPS-553 compute print size in pixels

--- packages/main/app/pages/print.vue
+++ packages/main/app/pages/print.vue
@@ -1,7 +1,15 @@
 <script lang="ts" setup>
+import { getPageSizeInPixels } from '../utils/printUtils';
+
 
 const { printConfig } = usePrintStore()
 console.log("printConfig", printConfig);
+
+if (printConfig) {
+    const pageSizePixel = getPageSizeInPixels(printConfig?.format, printConfig?.orientation, printConfig?.resolution)
+    console.log(">>> pageSizePixel", pageSizePixel);
+    
+}
 
 </script>
 
