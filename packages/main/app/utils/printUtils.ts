1e8136827066522c7f996589ea75330226543a1d
jonathanlurie <lurie.jo@gmail.com>
2026-04-14 14:03:19 +0200

    GPS-553 compute print size in pixels

---
+++ packages/main/app/utils/printUtils.ts
@@ -0,0 +1,28 @@
+import type { PrintOrientation, PrintFormat } from '@swissgeo/statesharing'
+
+const MM_PER_INCH = 25.4
+const SQRT_2 = 2 ** 0.5
+
+// Length in millimeter of the short side of the page
+const A0_SHORT_SIDE_MM = 841
+// Length in millimeter of the long side of the page
+const A0_LONG_SIDE_MM = 1189
+
+
+const formatRatiosToA0: Record<PrintFormat, number> = {
+    a0: SQRT_2 ** 0,
+    a1: SQRT_2 ** 1,
+    a2: SQRT_2 ** 2,
+    a3: SQRT_2 ** 3,
+    a4: SQRT_2 ** 4,
+    a5: SQRT_2 ** 5,
+}
+
+/**
+ * Get the print page size in pixels from a given format (eg. 'a4'), an orientation (eg. 'landscape') and a resolution in DPI (eg. 192)
+ */
+export function getPageSizeInPixels(format: PrintFormat, orientation: PrintOrientation, resolution: number): { width: number, height: number } {
+    const longSide = resolution * ~~(A0_LONG_SIDE_MM / formatRatiosToA0[format]) / MM_PER_INCH
+    const shortSide = resolution * ~~(A0_SHORT_SIDE_MM / formatRatiosToA0[format]) / MM_PER_INCH
+    return orientation === 'landscape' ? { width: longSide, height: shortSide } : { width: shortSide, height: longSide }
+}
--- packages/statesharing/src/index.ts
+++ packages/statesharing/src/index.ts
@@ -1,4 +1,4 @@
 export * from './stateConfig'
-export type { AppStateConfig, AppStatePayload, LayerStateConfig, PrintConfig } from './types/types'
+export type { AppStateConfig, AppStatePayload, LayerStateConfig, PrintConfig, PrintFormat, PrintOrientation } from './types/types'
 export { APP_STATE_CONFIG_VERSION } from './constants'
 export { validatePrintConfig } from './utils/validation'
