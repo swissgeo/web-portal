ae2e4eda5bb2e757bd73e068bf78f2f165c5644f
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-04-23 11:43:23 +0200

    GPS-619 update dependencies in pnpm-workspace.yaml

--- packages/config/prettier/index.js
+++ packages/config/prettier/index.js
@@ -1,6 +1,17 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var config = {
+var defaultConfig = {
     printWidth: 100,
     singleQuote: true,
     singleAttributePerLine: true,
@@ -8,12 +19,6 @@ var config = {
     trailingComma: 'es5',
     tabWidth: 4,
     jsxSingleQuote: false,
-    plugins: [
-        '@prettier/plugin-xml',
-        'prettier-plugin-jsdoc',
-        'prettier-plugin-packagejson',
-        'prettier-plugin-tailwindcss',
-    ],
     overrides: [
         {
             files: '*.md',
@@ -21,6 +26,29 @@ var config = {
                 tabWidth: 2,
             },
         },
+        {
+            files: '*.yml',
+            options: {
+                tabWidth: 2,
+            },
+        },
     ],
 };
-exports.default = config;
+/**
+ * Define the prettier config, given the plugins to use
+ *
+ * @param plugins A list of plugins to use. If you provide a plugin here, you need to have it
+ *   installed in your project (through your package.json file)
+ */
+function defineConfig() {
+    var plugins = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        plugins[_i] = arguments[_i];
+    }
+    var config = __assign({}, defaultConfig);
+    if (plugins) {
+        config.plugins = plugins;
+    }
+    return config;
+}
+exports.default = defineConfig;
