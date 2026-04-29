56b0b956184f7c2cdef82a0950e9a8fd960cd7e4
jonathanlurie <lurie.jo@gmail.com>
2026-04-29 11:02:00 +0200

    GPS-553 replaced official text with Lorem Ipsum + added print-ready signal for print service

--- packages/map/src/index.ts
+++ packages/map/src/index.ts
@@ -5,6 +5,8 @@ import useAddLayerToMap from './composables/useAddLayerToMap.composable'
 import MapModule from './MapModule.vue'
 import usePositionStore from './stores/position'
 
+export * from './stores/map.ts'
+
 // importing as "type" doesn't work with the DTS bundler somehow
 // eslint-disable-next-line @typescript-eslint/consistent-type-exports
 export * from '@/types/layers'
