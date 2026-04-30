8d7eb8762da51a0a4be2b9a598e6bba125cdf470
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-04-30 11:03:16 +0200

    GPS-679 update ElevationProfile component to handle loading state correctly

--- packages/elevation-profile/src/ElevationProfile.vue
+++ packages/elevation-profile/src/ElevationProfile.vue
@@ -2,22 +2,16 @@
 import type { ElevationProfileResponse } from '@swissgeo/shared/api'
 import ElevationProfilePlot from './ElevationProfilePlot.vue'
 
-const { profileResponse, isLoading } = defineProps<{
-    profileResponse?: ElevationProfileResponse
+defineProps<{
+    profileResponse: ElevationProfileResponse | undefined
     isLoading: boolean
 }>()
 </script>
 
 <template>
     <div>
-        <div
-            v-if="isLoading"
-            class="flex h-48 items-center justify-center"
-        >
-            Loading...
-        </div>
         <ElevationProfilePlot
-            v-else-if="profileResponse"
+            v-if="profileResponse"
             :profile="profileResponse"
         />
     </div>
