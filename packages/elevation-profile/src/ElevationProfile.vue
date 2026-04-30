ce248f47d3158938ad3335b40ff8b5c6f283f75d
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-04-30 16:27:28 +0200

    GPS-679 implement profile reversal and CSV export

--- packages/elevation-profile/src/ElevationProfile.vue
+++ packages/elevation-profile/src/ElevationProfile.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import type { ElevationProfileResponse } from '@swissgeo/shared/api'
 
 import ElevationProfileMetadata from './components/ElevationProfileMetadata.vue'
@@ -7,7 +8,7 @@ import ElevationProfilePlot from './components/ElevationProfilePlot.vue'
 import type { Labels as PlotLabels } from './components/ElevationProfilePlot.vue'
 import type { Labels as MetadataLabels } from './components/ElevationProfileMetadata.vue'
 
-defineProps<{
+const props = defineProps<{
     profileResponse?: ElevationProfileResponse
     isLoading: boolean
 }>()
@@ -31,22 +32,75 @@ const labels: {
         slopeDistance: 'Path distance',
     },
 }
+
+const isReverse = ref<boolean>(false)
+const profile = computed<ElevationProfileResponse | undefined>(() => {
+    if (!props.profileResponse) {
+        return
+    }
+    return isReverse.value ? reverseProfile(props.profileResponse) : props.profileResponse
+})
+
+function reverseProfile(profile: ElevationProfileResponse): ElevationProfileResponse {
+    const reversedProfile = {
+        ...profile,
+        metadata: {
+            ...profile.metadata,
+            elevationDifference: -profile.metadata.elevationDifference,
+            totalAscent: profile.metadata.totalDescent,
+            totalDescent: profile.metadata.totalAscent,
+            maxElevation: profile.metadata.maxElevation,
+            minElevation: profile.metadata.minElevation,
+        },
+        points: [...profile.points].reverse(),
+    }
+    return reversedProfile
+}
+
+function exportCSV(profile: ElevationProfileResponse): void {
+    const csvContent =
+        'data:text/csv;charset=utf-8,' +
+        ['distance,elevation', ...profile.points.map((p) => `${p.dist},${p.elevation}`)].join('\n')
+    const encodedUri = encodeURI(csvContent)
+    const link = document.createElement('a')
+    link.setAttribute('href', encodedUri)
+    link.setAttribute('download', 'elevation_profile.csv')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
 </script>
 
 <template>
     <div class="flex w-full flex-col items-center justify-center">
         <ElevationProfilePlot
             v-if="profileResponse"
-            :profile="profileResponse"
+            :profile="profile"
             :labels="labels.plot"
         >
             <slot />
         </ElevationProfilePlot>
         <ElevationProfileMetadata
             v-if="profileResponse"
-            :metadata="profileResponse.metadata"
+            :metadata="profile.metadata"
             :labels="labels.metadata"
-        />
+        >
+            <UButton
+                icon="i-lucide-arrow-left-right"
+                size="md"
+                color="primary"
+                variant="solid"
+                @click="isReverse = !isReverse"
+            />
+
+            <UButton
+                icon="i-lucide-download"
+                size="md"
+                color="primary"
+                variant="solid"
+                @click="exportCSV(profile)"
+            />
+        </ElevationProfileMetadata>
     </div>
 </template>
 
