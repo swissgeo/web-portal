851901c7eb2991228f193375d3b0e6f7fefd5520
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-04-30 18:15:43 +0200

    GPS-664 Fix some types

--- packages/main/app/utils/fetchStateFromStateId.ts
+++ packages/main/app/utils/fetchStateFromStateId.ts
@@ -1,12 +1,15 @@
 /**
  * Retrieve state from service-shortlink
  */
-export async function fetchStateFromStateId(stateId: string): Promise<JSON | null> {
+export async function fetchStateFromStateId(
+    stateId: string
+): Promise<Record<string, unknown> | null> {
+    // not using the state type yet as we haven't validated it yet
     const runtimeConfig = useRuntimeConfig()
     const shortLinkUrl = new URL(runtimeConfig.public.shareServiceUrl)
     shortLinkUrl.searchParams.set('state', stateId)
 
-    const appConfig = await $fetch<JSON>(shortLinkUrl.toString())
+    const appConfig = await $fetch<Record<string, unknown>>(shortLinkUrl.toString())
 
     return appConfig
 }
