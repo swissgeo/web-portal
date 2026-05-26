7b2a9bedaff7d8db013c787224f1ba3263e9b6ab
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-05-26 15:01:28 +0200

    GPS-585 remove unused state configuration and validation files

--- packages/statesharing/src/index.ts
+++ packages/statesharing/src/index.ts
@@ -1,7 +1,11 @@
-export * from "./stateConfig";
-export type {
-  AppStateConfig,
-  AppStatePayload,
-  LayerStateConfig,
-} from "./types/types";
 export { APP_STATE_CONFIG_VERSION } from "./constants";
+export { GetAppStateResponse as ReadAppStateValidator } from "./client/schemas";
+export { SaveAppStateRequest as SaveAppStateValidator } from "./client/schemas";
+export { StateV1Input as StatePayloadValidator } from "./client/schemas";
+export type {
+  SaveAppStateRequest as SaveAppState,
+  GetAppStateResponse as GetAppState,
+  LayerStateOutput as LayerState,
+  MapState,
+} from "./client/schemas";
+export type { StateV1Input as AppState } from "./client/schemas";
