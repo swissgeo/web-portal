b2c7ff637f3b59a0f91193f29dd0c167ff914c7a
Jonathan Lurie <lurie.jo@gmail.com>
2026-05-27 17:39:36 +0200

    GPS-730 lint and format

--- packages/main/app/composables/__tests__/useUrlParams.nuxt.spec.ts
+++ packages/main/app/composables/__tests__/useUrlParams.nuxt.spec.ts
@@ -1,143 +1,144 @@
-import { mockNuxtImport } from '@nuxt/test-utils/runtime'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { mockNuxtImport } from "@nuxt/test-utils/runtime";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 
 const { useRouteMock, useRouterMock, fetchStateMock } = vi.hoisted(() => ({
-    useRouteMock: {
-        query: {
-            state: 'ABC',
-        },
-    } as {
-        query: {
-            state?: string | string[]
-        }
+  useRouteMock: {
+    query: {
+      state: "ABC",
     },
-    useRouterMock: {
-        beforeEach: vi.fn(),
-        afterEach: vi.fn(),
-        beforeResolve: vi.fn(),
-        onError: vi.fn(),
-        replace: vi.fn(),
-    },
-    fetchStateMock: vi.fn(() => Promise.resolve('success')),
-}))
+  } as {
+    query: {
+      state?: string | string[];
+    };
+  },
+  useRouterMock: {
+    beforeEach: vi.fn(),
+    afterEach: vi.fn(),
+    beforeResolve: vi.fn(),
+    onError: vi.fn(),
+    replace: vi.fn(),
+  },
+  fetchStateMock: vi.fn(() => Promise.resolve("success")),
+}));
 
 // Mock navigateTo
-mockNuxtImport('navigateTo', () => vi.fn())
+mockNuxtImport("navigateTo", () => vi.fn());
 
 // immediately invoke the callback
-mockNuxtImport('onNuxtReady', () => {
-    return (fn: () => void) => fn()
-})
+mockNuxtImport("onNuxtReady", () => {
+  return (fn: () => void) => fn();
+});
 
-mockNuxtImport('useRouter', () => {
-    return () => ({
-        ...useRouterMock,
-    })
-})
+mockNuxtImport("useRouter", () => {
+  return () => ({
+    ...useRouterMock,
+  });
+});
 
-mockNuxtImport('useRoute', () => {
-    return () => ({
-        ...useRouteMock,
-    })
-})
+mockNuxtImport("useRoute", () => {
+  return () => ({
+    ...useRouteMock,
+  });
+});
 
-vi.mock('~/utils/fetchStateFromStateId', () => ({
-    fetchStateFromStateId: fetchStateMock,
-}))
+vi.mock("~/utils/fetchStateFromStateId", () => ({
+  fetchStateFromStateId: fetchStateMock,
+}));
 
-describe('useUrlParams state extraction', () => {
-    beforeEach(() => {
-        vi.clearAllMocks()
-    })
+describe("useUrlParams state extraction", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-    // for some reason, if this isn't the first test in the sequence, it will fail
-    // I tried increasing the timeout, didn't really help. I suspect there is something
-    // fishy with our test setup, and created GPS-629 to investigate that
-    it('Calls the fetchState util', async () => {
-        const { getStateFromUrl } = useUrlParams()
-        await getStateFromUrl()
-        expect(fetchStateMock).toHaveBeenLastCalledWith('ABC')
-    })
+  // for some reason, if this isn't the first test in the sequence, it will fail
+  // I tried increasing the timeout, didn't really help. I suspect there is something
+  // fishy with our test setup, and created GPS-629 to investigate that
+  it("Calls the fetchState util", async () => {
+    const { getStateFromUrl } = useUrlParams();
+    await getStateFromUrl();
+    expect(fetchStateMock).toHaveBeenLastCalledWith("ABC");
+  });
 
-    it('Extracts the state id from the URL correctly', () => {
-        const { getStateIdFromUrl } = useUrlParams()
+  it("Extracts the state id from the URL correctly", () => {
+    const { getStateIdFromUrl } = useUrlParams();
 
-        const stateId = getStateIdFromUrl()
-        expect(stateId).toEqual('ABC')
-    })
+    const stateId = getStateIdFromUrl();
+    expect(stateId).toEqual("ABC");
+  });
 
-    it('Extracts the first state param if there are multiple', () => {
-        useRouteMock.query.state = ['DEF', 'GHI']
-        const { getStateIdFromUrl } = useUrlParams()
+  it("Extracts the first state param if there are multiple", () => {
+    useRouteMock.query.state = ["DEF", "GHI"];
+    const { getStateIdFromUrl } = useUrlParams();
 
-        const stateId = getStateIdFromUrl()
-        expect(stateId).toEqual('DEF')
-    })
+    const stateId = getStateIdFromUrl();
+    expect(stateId).toEqual("DEF");
+  });
 
-    it("Returns null if there's no state param", () => {
-        useRouteMock.query = {
-            // @ts-expect-error Specifying another param to test the absence
-            anotherParam: 'value',
-        }
-        const { getStateIdFromUrl } = useUrlParams()
+  it("Returns null if there's no state param", () => {
+    useRouteMock.query = {
+      // @ts-expect-error Specifying another param to test the absence
+      anotherParam: "value",
+    };
+    const { getStateIdFromUrl } = useUrlParams();
 
-        const stateId = getStateIdFromUrl()
-        expect(stateId).toBeNull()
-    })
+    const stateId = getStateIdFromUrl();
+    expect(stateId).toBeNull();
+  });
 
-    it("Returns null if the state id isn't a string", () => {
-        useRouteMock.query = {
-            // @ts-expect-error Intentionally breaking the type here
-            state: 3,
-        }
-        const { getStateIdFromUrl } = useUrlParams()
+  it("Returns null if the state id isn't a string", () => {
+    useRouteMock.query = {
+      // @ts-expect-error Intentionally breaking the type here
+      state: 3,
+    };
+    const { getStateIdFromUrl } = useUrlParams();
 
-        const stateId = getStateIdFromUrl()
-        expect(stateId).toBeNull()
-    })
+    const stateId = getStateIdFromUrl();
+    expect(stateId).toBeNull();
+  });
 
-    it("Returns a promise with null if the's no state param", async () => {
-        useRouteMock.query = {
-            // @ts-expect-error Specifying another param to test the absence
-            anotherParam: 'value',
-        }
-        const { getStateFromUrl } = useUrlParams()
-        const getStateSpy = vi.fn(getStateFromUrl)
-        const state = await getStateSpy()
-        expect(getStateSpy).toHaveResolved()
-        expect(state).toMatchObject({ state: null, stateId: null })
-    })
-})
+  it("Returns a promise with null if the's no state param", async () => {
+    useRouteMock.query = {
+      // @ts-expect-error Specifying another param to test the absence
+      anotherParam: "value",
+    };
+    const { getStateFromUrl } = useUrlParams();
+    const getStateSpy = vi.fn(getStateFromUrl);
+    const state = await getStateSpy();
+    expect(getStateSpy).toHaveResolved();
+    expect(state).toMatchObject({ state: null, stateId: null });
+  });
+});
 
-describe('useUrlParam base64 state extraction', () => {
-    beforeEach(() => {
-        useRouteMock.query = {
-            // intentionally providing a fixed value here. The content is irrelevant for this test
-            state: 'ewogICJ2ZXJzaW9uIjogIjEuMCIsCiAgInN0YXRlIjogewogICAgIm1hcCI6IHsKICAgICAgImNlbnRlciI6IFsKICAgICAgICAyNjYwMDAwLAogICAgICAgIDExOTAwMDAKICAgICAgXQogICB9CiAgfQp9',
-        }
+describe("useUrlParam base64 state extraction", () => {
+  beforeEach(() => {
+    useRouteMock.query = {
+      // intentionally providing a fixed value here. The content is irrelevant for this test
+      state:
+        "ewogICJ2ZXJzaW9uIjogIjEuMCIsCiAgInN0YXRlIjogewogICAgIm1hcCI6IHsKICAgICAgImNlbnRlciI6IFsKICAgICAgICAyNjYwMDAwLAogICAgICAgIDExOTAwMDAKICAgICAgXQogICB9CiAgfQp9",
+    };
 
-        vi.clearAllMocks()
-    })
+    vi.clearAllMocks();
+  });
 
-    it('extracts a state from base64', async () => {
-        const { getStateFromUrl } = useUrlParams()
-        const { state } = await getStateFromUrl()
-        expect(state).toEqual({
-            version: '1.0',
-            state: {
-                map: {
-                    center: [2660000, 1190000],
-                },
-            },
-        })
-    })
+  it("extracts a state from base64", async () => {
+    const { getStateFromUrl } = useUrlParams();
+    const { state } = await getStateFromUrl();
+    expect(state).toEqual({
+      version: "1.0",
+      state: {
+        map: {
+          center: [2660000, 1190000],
+        },
+      },
+    });
+  });
 
-    it('drops the parameter after importing the base64 state', async () => {
-        const { getStateFromUrl } = useUrlParams()
-        expect(useRouteMock.query).toHaveProperty('state')
+  it("drops the parameter after importing the base64 state", async () => {
+    const { getStateFromUrl } = useUrlParams();
+    expect(useRouteMock.query).toHaveProperty("state");
 
-        const { state } = await getStateFromUrl()
-        expect(state).toBeTruthy()
-        expect(useRouterMock.replace).toHaveBeenCalledWith({ query: {} })
-    })
-})
+    const { state } = await getStateFromUrl();
+    expect(state).toBeTruthy();
+    expect(useRouterMock.replace).toHaveBeenCalledWith({ query: {} });
+  });
+});
--- packages/main/app/composables/useUrlParams.ts
+++ packages/main/app/composables/useUrlParams.ts
@@ -1,108 +1,110 @@
-import log, { LogPreDefinedColor } from '@swissgeo/log'
-import { fetchStateFromStateId } from '~/utils/fetchStateFromStateId'
+import log, { LogPreDefinedColor } from "@swissgeo/log";
+import { fetchStateFromStateId } from "~/utils/fetchStateFromStateId";
 
-import type { PrintConfig } from '../types/print'
+import type { PrintConfig } from "../types/print";
 
 // URL param providing the ID to a state on service-portal-state
-export const URL_PARAM_STATE = 'state'
+export const URL_PARAM_STATE = "state";
 
 // URL param related to print config
-const URL_PARAM_PRINT_FORMAT = 'print_format'
-const URL_PARAM_PRINT_ORIENTATION = 'print_orientation'
-const URL_PARAM_PRINT_RESOLUTION = 'print_resolution'
+const URL_PARAM_PRINT_FORMAT = "print_format";
+const URL_PARAM_PRINT_ORIENTATION = "print_orientation";
+const URL_PARAM_PRINT_RESOLUTION = "print_resolution";
 
 export function useUrlParams() {
-    const route = useRoute()
-    const router = useRouter()
+  const route = useRoute();
+  const router = useRouter();
 
-    function dropStateParam(paramName: typeof URL_PARAM_STATE) {
-        // remove the state ID from the URL (without page refresh)
-        onNuxtReady(async () => {
-            const newQuery = { ...route.query }
-            delete newQuery[paramName]
-            await router.replace({ query: newQuery })
-        })
+  function dropStateParam(paramName: typeof URL_PARAM_STATE) {
+    // remove the state ID from the URL (without page refresh)
+    onNuxtReady(async () => {
+      const newQuery = { ...route.query };
+      delete newQuery[paramName];
+      await router.replace({ query: newQuery });
+    });
+  }
+
+  /**
+   * Get the state ID from URL, or null is missing
+   */
+  function getStateIdFromUrl(): string | null {
+    const stateParam = route.query[URL_PARAM_STATE];
+
+    if (!stateParam) {
+      return null;
     }
 
-    /**
-     * Get the state ID from URL, or null is missing
-     */
-    function getStateIdFromUrl(): string | null {
-        const stateParam = route.query[URL_PARAM_STATE]
+    dropStateParam(URL_PARAM_STATE);
 
-        if (!stateParam) {
-            return null
-        }
+    // Extract the value, handling the case where multiple params with the same name exist
+    const stateId = Array.isArray(stateParam) ? stateParam[0] : stateParam;
 
-        dropStateParam(URL_PARAM_STATE)
-
-        // Extract the value, handling the case where multiple params with the same name exist
-        const stateId = Array.isArray(stateParam) ? stateParam[0] : stateParam
-
-        if (typeof stateId !== 'string') {
-            return null
-        }
-
-        log.debug({
-            title: 'useUrlParam',
-            titleColor: LogPreDefinedColor.Sky,
-            messages: [
-                'State found in the URL param, using this to fetch the state from the service',
-                stateId,
-            ],
-        })
-
-        return stateId
+    if (typeof stateId !== "string") {
+      return null;
     }
 
-    /**
-     * Read the state ID from URL param, load the state corresponding to this ID,
-     * return it as a payload and removed the param from the URL
-     */
-    async function getStateFromUrl(): Promise<{
-        state: unknown
-        stateId: string | null
-    }> {
-        const stateId = getStateIdFromUrl()
-        if (!stateId) {
-            return Promise.resolve({
-                state: null,
-                stateId: null,
-            })
-        }
+    log.debug({
+      title: "useUrlParam",
+      titleColor: LogPreDefinedColor.Sky,
+      messages: [
+        "State found in the URL param, using this to fetch the state from the service",
+        stateId,
+      ],
+    });
 
-        let state: Record<string, unknown> | null = null
+    return stateId;
+  }
 
-        try {
-            state = JSON.parse(atob(stateId))
-        } catch {
-            state = await fetchStateFromStateId(stateId)
-        }
-
-        return {
-            state,
-            stateId,
-        }
+  /**
+   * Read the state ID from URL param, load the state corresponding to this ID,
+   * return it as a payload and removed the param from the URL
+   */
+  async function getStateFromUrl(): Promise<{
+    state: unknown;
+    stateId: string | null;
+  }> {
+    const stateId = getStateIdFromUrl();
+    if (!stateId) {
+      return Promise.resolve({
+        state: null,
+        stateId: null,
+      });
     }
 
-    /**
-     * Get the print config from the URL
-     * ?print_format=a4&print_orientation=landscape&print_resolution=96&z=8
-     */
-    function getPrintConfigFromUrl(): PrintConfig {
-        const printConfig = {
-            format: route.query[URL_PARAM_PRINT_FORMAT],
-            orientation: route.query[URL_PARAM_PRINT_ORIENTATION],
-            resolution: Number.parseFloat(route.query[URL_PARAM_PRINT_RESOLUTION] as string),
-        }
+    let state: Record<string, unknown> | null = null;
 
-        validatePrintConfig(printConfig)
-        return printConfig
+    try {
+      state = JSON.parse(atob(stateId));
+    } catch {
+      state = await fetchStateFromStateId(stateId);
     }
 
     return {
-        getStateFromUrl,
-        getStateIdFromUrl,
-        getPrintConfigFromUrl,
-    }
+      state,
+      stateId,
+    };
+  }
+
+  /**
+   * Get the print config from the URL
+   * ?print_format=a4&print_orientation=landscape&print_resolution=96&z=8
+   */
+  function getPrintConfigFromUrl(): PrintConfig {
+    const printConfig = {
+      format: route.query[URL_PARAM_PRINT_FORMAT],
+      orientation: route.query[URL_PARAM_PRINT_ORIENTATION],
+      resolution: Number.parseFloat(
+        route.query[URL_PARAM_PRINT_RESOLUTION] as string,
+      ),
+    };
+
+    validatePrintConfig(printConfig);
+    return printConfig;
+  }
+
+  return {
+    getStateFromUrl,
+    getStateIdFromUrl,
+    getPrintConfigFromUrl,
+  };
 }
