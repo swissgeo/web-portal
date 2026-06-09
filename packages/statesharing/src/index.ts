675d4c0999a6aac4ef40b14c4b8453922f1aa88a
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-06-09 11:12:17 +0200

    GPS-585 generate client

--- packages/statesharing/src/index.ts
+++ packages/statesharing/src/index.ts
@@ -1,13 +1,13 @@
 export { APP_STATE_CONFIG_VERSION } from "./constants";
-export { GetAppStateResponse as ReadAppStateValidator } from "./client/schemas";
-export { SaveAppStateRequest as SaveAppStateValidator } from "./client/schemas";
-export { StateV1Input as StatePayloadValidator } from "./client/schemas";
-export { SaveAppStateResponse } from "./client/schemas";
+export { zGetAppStateResponse as ReadAppStateValidator } from "./hey-api/zod.gen";
+export { zSaveAppStateRequest as SaveAppStateValidator } from "./hey-api/zod.gen";
+export { zStateV1Input as StatePayloadValidator } from "./hey-api/zod.gen";
+export { zSaveAppStateResponse as SaveAppStateResponse } from "./hey-api/zod.gen";
 export type {
   SaveAppStateRequest as SaveAppState,
   GetAppStateResponse as GetAppState,
   LayerStateOutput as LayerState,
   LayerStateInput,
   MapState,
-} from "./client/schemas";
-export type { StateV1Input as AppState } from "./client/schemas";
+} from "./hey-api/types.gen";
+export type { StateV1Input as AppState } from "./hey-api/types.gen";
