f2d9be4ae7b5b7c1c42452763bc28f111b161c46
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-06-09 20:54:24 +0200

    Migrate shared to rolldown

---
+++ packages/shared/rolldown.config.js
@@ -0,0 +1,18 @@
+// rolldown.config.js
+import { dts } from "rolldown-plugin-dts";
+
+const isDev = process.env.NODE_ENV === "development";
+
+export default {
+  input: "./src/index.ts",
+  plugins: [dts()],
+  output: [
+    {
+      dir: "dist",
+      format: "es",
+      minify: !isDev,
+      sourcemap: isDev,
+    },
+  ],
+  external: ["nuxt", "@swissgeo/numbers", "vue"],
+};
