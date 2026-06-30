4bf5c04e6916942d14c24dcb7f57c6131dea187f
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-06-30 13:24:48 +0200

    GPS-809 refactor using a shared mock for position store

--- packages/map/src/stores/__tests__/position/decreaseZoom.spec.ts
+++ packages/map/src/stores/__tests__/position/decreaseZoom.spec.ts
@@ -1,16 +1,9 @@
 import type { ActionDispatcher } from "@swissgeo/shared/action-dispatcher";
 
-import { createPinia, setActivePinia } from "pinia";
 import { describe, it, expect } from "vitest";
 
-import usePositionStore from "@/stores/position";
-
+import { positionStore } from "../__mocks__/stores";
 import decreaseZoom from "../../position/actions/decreaseZoom";
-
-const pinia = createPinia();
-setActivePinia(pinia); // Sets the active instance manually
-
-const store = usePositionStore();
 
 const mockDispatcher: ActionDispatcher = {
   name: "mockDispatcher",
@@ -18,8 +11,8 @@ const mockDispatcher: ActionDispatcher = {
 
 describe("decreaseZoom", () => {
   it("should decrease the zoom level by 1", () => {
-    const initialZoom = store.zoom;
-    decreaseZoom.call(store, mockDispatcher);
-    expect(store.zoom).toBe(initialZoom - 1);
+    const initialZoom = positionStore.zoom;
+    decreaseZoom.call(positionStore, mockDispatcher);
+    expect(positionStore.zoom).toBe(initialZoom - 1);
   });
 });
--- packages/map/src/stores/__tests__/position/increaseZoom.spec.ts
+++ packages/map/src/stores/__tests__/position/increaseZoom.spec.ts
@@ -1,16 +1,9 @@
 import type { ActionDispatcher } from "@swissgeo/shared/action-dispatcher";
 
-import { createPinia, setActivePinia } from "pinia";
 import { describe, it, expect } from "vitest";
 
-import usePositionStore from "@/stores/position";
-
+import { positionStore } from "../__mocks__/stores";
 import increaseZoom from "../../position/actions/increaseZoom";
-
-const pinia = createPinia();
-setActivePinia(pinia); // Sets the active instance manually
-
-const store = usePositionStore();
 
 const mockDispatcher: ActionDispatcher = {
   name: "mockDispatcher",
@@ -18,8 +11,8 @@ const mockDispatcher: ActionDispatcher = {
 
 describe("increaseZoom", () => {
   it("should increase the zoom level by 1", () => {
-    const initialZoom = store.zoom;
-    increaseZoom.call(store, mockDispatcher);
-    expect(store.zoom).toBe(initialZoom + 1);
+    const initialZoom = positionStore.zoom;
+    increaseZoom.call(positionStore, mockDispatcher);
+    expect(positionStore.zoom).toBe(initialZoom + 1);
   });
 });
