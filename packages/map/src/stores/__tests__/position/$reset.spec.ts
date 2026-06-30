0f55a450a20ed77b8588ac23fdf89a08ecb3a0fe
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-06-30 13:25:02 +0200

    GPS-809 add tests for $reset

---
+++ packages/map/src/stores/__tests__/position/$reset.spec.ts
@@ -0,0 +1,34 @@
+import type { ActionDispatcher } from "@swissgeo/shared/action-dispatcher";
+
+import { describe, it, expect } from "vitest";
+
+import { LV03Format } from "@/utils/coordinates/coordinateFormat";
+
+import { positionStore } from "../__mocks__/stores";
+import { DEFAULT_PROJECTION, DEFAULT_FORMAT } from "../../position";
+import $reset from "../../position/actions/$reset";
+
+const mockDispatcher: ActionDispatcher = {
+  name: "mockDispatcher",
+};
+
+describe("$reset", () => {
+  it("should reset the store to its initial state", () => {
+    positionStore.center = [600000, 200000];
+    positionStore.zoom = 5;
+    positionStore.rotation = 45;
+    positionStore.displayFormat = LV03Format;
+
+    expect(positionStore.center).not.toEqual(DEFAULT_PROJECTION.bounds.center);
+    expect(positionStore.zoom).not.toBe(DEFAULT_PROJECTION.getDefaultZoom());
+    expect(positionStore.rotation).not.toBe(0);
+    expect(positionStore.displayFormat).not.toStrictEqual(DEFAULT_FORMAT);
+
+    $reset.call(positionStore, mockDispatcher);
+
+    expect(positionStore.center).toEqual(DEFAULT_PROJECTION.bounds.center);
+    expect(positionStore.zoom).toBe(DEFAULT_PROJECTION.getDefaultZoom());
+    expect(positionStore.rotation).toBe(0);
+    expect(positionStore.displayFormat).toStrictEqual(DEFAULT_FORMAT);
+  });
+});
