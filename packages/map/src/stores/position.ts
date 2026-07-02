550f4c7263cbfd11852721f0ef0b8eeba36cb2ab
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-07-02 11:33:54 +0200

    GPS-811 migrate from an options store to a setup one

--- packages/map/src/stores/position.ts
+++ packages/map/src/stores/position.ts
@@ -6,6 +6,7 @@ import log, { LogPreDefinedColor } from "@swissgeo/log";
 import { isNumber } from "@swissgeo/numbers";
 import { defineStore } from "pinia";
 import proj4 from "proj4";
+import { computed, ref } from "vue";
 
 import type { CoordinateFormat } from "@/utils/coordinates/coordinateFormat";
 
@@ -15,118 +16,141 @@ import { normalizeAngle } from "@/utils/normalizeAngle";
 export const DEFAULT_PROJECTION: CoordinateSystem = LV95;
 export const DEFAULT_FORMAT = LV95Format;
 
-const state = () => ({
-  displayFormat: DEFAULT_FORMAT,
-  zoom: DEFAULT_PROJECTION.getDefaultZoom(),
-  rotation: 0,
-  autoRotation: false,
-  center: DEFAULT_PROJECTION.bounds.center,
-  projection: DEFAULT_PROJECTION,
-});
+const usePositionStore = defineStore("position", () => {
+  const displayFormat = ref<CoordinateFormat>(DEFAULT_FORMAT);
+  const zoom = ref(DEFAULT_PROJECTION.getDefaultZoom());
+  const rotation = ref(0);
+  const autoRotation = ref(false);
+  const center = ref<SingleCoordinate>(DEFAULT_PROJECTION.bounds.center);
+  const projection = ref<CoordinateSystem>(DEFAULT_PROJECTION);
 
-const usePositionStore = defineStore("position", {
-  state,
-  getters: {
-    centerEpsg4326(): SingleCoordinate {
-      const centerEpsg4326Unrounded = proj4(
-        this.projection.epsg,
-        WGS84.epsg,
-        this.center,
-      );
-      return [
-        WGS84.roundCoordinateValue(centerEpsg4326Unrounded[0]),
-        WGS84.roundCoordinateValue(centerEpsg4326Unrounded[1]),
-      ];
-    },
-    resolution(): number {
-      return this.projection.getResolutionForZoom(this.zoom, this.center);
-    },
-  },
-  actions: {
-    setDisplayedFormat(
-      displayedFormat: CoordinateFormat,
-      dispatcher: ActionDispatcher,
-    ): void {
-      this.displayFormat = displayedFormat;
-    },
+  const centerEpsg4326 = computed<SingleCoordinate>(() => {
+    const centerEpsg4326Unrounded = proj4(
+      projection.value.epsg,
+      WGS84.epsg,
+      center.value,
+    );
+    return [
+      WGS84.roundCoordinateValue(centerEpsg4326Unrounded[0]),
+      WGS84.roundCoordinateValue(centerEpsg4326Unrounded[1]),
+    ];
+  });
 
-    setZoom(zoom: number, dispatcher: ActionDispatcher): void {
-      if (!isNumber(zoom) || zoom < 0) {
-        log.error({
-          title: "Position store / setZoom",
-          titleColor: LogPreDefinedColor.Red,
-          messages: ["Invalid zoom level", zoom, dispatcher],
-        });
-        return;
-      }
-      this.zoom = this.projection.roundZoomLevel(zoom);
-    },
+  const resolution = computed(() =>
+    projection.value.getResolutionForZoom(zoom.value, center.value),
+  );
 
-    increaseZoom(dispatcher: ActionDispatcher): void {
-      if (this.projection instanceof SwissCoordinateSystem) {
-        this.zoom = this.projection.roundZoomLevel(this.zoom, true) + 1;
-      } else {
-        this.zoom = this.projection.roundZoomLevel(this.zoom) + 1;
-      }
-    },
+  function setDisplayedFormat(
+    newFormat: CoordinateFormat,
+    dispatcher: ActionDispatcher,
+  ): void {
+    displayFormat.value = newFormat;
+  }
 
-    decreaseZoom(dispatcher: ActionDispatcher): void {
-      if (this.projection instanceof SwissCoordinateSystem) {
-        this.zoom = this.projection.roundZoomLevel(this.zoom, true) - 1;
-      } else {
-        this.zoom = this.projection.roundZoomLevel(this.zoom) - 1;
-      }
-    },
+  function setZoom(newZoom: number, dispatcher: ActionDispatcher): void {
+    if (!isNumber(newZoom) || newZoom < 0) {
+      log.error({
+        title: "Position store / setZoom",
+        titleColor: LogPreDefinedColor.Red,
+        messages: ["Invalid zoom level", newZoom, dispatcher],
+      });
+      return;
+    }
+    zoom.value = projection.value.roundZoomLevel(newZoom);
+  }
 
-    setRotation(rotation: number, dispatcher: ActionDispatcher): void {
-      if (!isNumber(rotation)) {
-        log.error({
-          title: "Position store / setRotation",
-          titleColor: LogPreDefinedColor.Red,
-          messages: ["Invalid rotation", rotation, dispatcher],
-        });
-        return;
-      }
-      this.rotation = normalizeAngle(rotation);
-    },
+  function increaseZoom(dispatcher: ActionDispatcher): void {
+    if (projection.value instanceof SwissCoordinateSystem) {
+      zoom.value = projection.value.roundZoomLevel(zoom.value, true) + 1;
+    } else {
+      zoom.value = projection.value.roundZoomLevel(zoom.value) + 1;
+    }
+  }
 
-    setAutoRotation(autoRotation: boolean, dispatcher: ActionDispatcher): void {
-      this.autoRotation = autoRotation;
-    },
+  function decreaseZoom(dispatcher: ActionDispatcher): void {
+    if (projection.value instanceof SwissCoordinateSystem) {
+      zoom.value = projection.value.roundZoomLevel(zoom.value, true) - 1;
+    } else {
+      zoom.value = projection.value.roundZoomLevel(zoom.value) - 1;
+    }
+  }
 
-    setCenter(center: SingleCoordinate, dispatcher: ActionDispatcher): void {
-      if (!center || (Array.isArray(center) && center.length !== 2)) {
-        log.error({
-          title: "Position store / setCenter",
-          titleColor: LogPreDefinedColor.Red,
-          messages: ["Invalid center, ignoring", center, dispatcher],
-        });
-        return;
-      }
+  function setRotation(
+    newRotation: number,
+    dispatcher: ActionDispatcher,
+  ): void {
+    if (!isNumber(newRotation)) {
+      log.error({
+        title: "Position store / setRotation",
+        titleColor: LogPreDefinedColor.Red,
+        messages: ["Invalid rotation", newRotation, dispatcher],
+      });
+      return;
+    }
+    rotation.value = normalizeAngle(newRotation);
+  }
 
-      if (this.projection.isInBounds(center)) {
-        this.center = center;
-      } else {
-        log.warn({
-          title: "Position store / setCenter",
-          titleColor: LogPreDefinedColor.Red,
-          messages: [
-            "Center received is out of projection bounds, ignoring",
-            this.projection,
-            center,
-            dispatcher,
-          ],
-        });
-      }
-    },
+  function setAutoRotation(
+    newAutoRotation: boolean,
+    dispatcher: ActionDispatcher,
+  ): void {
+    autoRotation.value = newAutoRotation;
+  }
 
-    $reset(dispatcher: ActionDispatcher): void {
-      this.setCenter(DEFAULT_PROJECTION.bounds.center, dispatcher);
-      this.setZoom(DEFAULT_PROJECTION.getDefaultZoom(), dispatcher);
-      this.setRotation(0, dispatcher);
-      this.setDisplayedFormat(DEFAULT_FORMAT, dispatcher);
-    },
-  },
+  function setCenter(
+    newCenter: SingleCoordinate,
+    dispatcher: ActionDispatcher,
+  ): void {
+    if (!newCenter || (Array.isArray(newCenter) && newCenter.length !== 2)) {
+      log.error({
+        title: "Position store / setCenter",
+        titleColor: LogPreDefinedColor.Red,
+        messages: ["Invalid center, ignoring", newCenter, dispatcher],
+      });
+      return;
+    }
+
+    if (projection.value.isInBounds(newCenter)) {
+      center.value = newCenter;
+    } else {
+      log.warn({
+        title: "Position store / setCenter",
+        titleColor: LogPreDefinedColor.Red,
+        messages: [
+          "Center received is out of projection bounds, ignoring",
+          projection.value,
+          newCenter,
+          dispatcher,
+        ],
+      });
+    }
+  }
+
+  function $reset(dispatcher: ActionDispatcher): void {
+    setCenter(DEFAULT_PROJECTION.bounds.center, dispatcher);
+    setZoom(DEFAULT_PROJECTION.getDefaultZoom(), dispatcher);
+    setRotation(0, dispatcher);
+    setDisplayedFormat(DEFAULT_FORMAT, dispatcher);
+  }
+
+  return {
+    displayFormat,
+    zoom,
+    rotation,
+    autoRotation,
+    center,
+    projection,
+    centerEpsg4326,
+    resolution,
+    setDisplayedFormat,
+    setZoom,
+    increaseZoom,
+    decreaseZoom,
+    setRotation,
+    setAutoRotation,
+    setCenter,
+    $reset,
+  };
 });
 
 export default usePositionStore;
