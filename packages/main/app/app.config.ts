20c0786a84b3fd4e1b5413f6d7b5086cc406d442
Raphael Antonietti <raphael.antonietti@swisstopo.ch>
2026-08-07 19:32:06 +0200

    chore: Clean design token configuration

--- packages/main/app/app.config.ts
+++ packages/main/app/app.config.ts
@@ -9,5 +9,15 @@ export default defineAppConfig({
       error: "red",
       neutral: "slate",
     },
+    button: {
+      compoundVariants: [
+        {
+          color: "primary",
+          variant: "solid",
+          class:
+            "bg-(--ui-btn-default) hover:bg-(--ui-btn-hover) disabled:bg-(--ui-btn-disabled) disabled:opacity-100 aria-disabled:bg-(--ui-btn-disabled) aria-disabled:opacity-100",
+        },
+      ],
+    },
   },
 });
