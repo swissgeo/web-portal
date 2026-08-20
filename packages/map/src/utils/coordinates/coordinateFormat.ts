b44cdd78fde7806ec3cf7f20266dd5bf8faa3462
Ismail Sunni <imajimatika@gmail.com>
2026-08-20 14:11:37 +0700

    GPS-904: move the MGRS/UTM conversions to @swissgeo/coordinates
    They are needed by packages that cannot depend on @swissgeo/map, such as the
    search. Also adds utmToLonLat, the counterpart of the existing latLonToUTM.

--- packages/map/src/utils/coordinates/coordinateFormat.ts
+++ packages/map/src/utils/coordinates/coordinateFormat.ts
@@ -1,7 +1,13 @@
-import type { CoordinateSystem, SingleCoordinate } from "@swissgeo/coordinates";
+import type {
+  CoordinateSystem,
+  SingleCoordinate,
+  UTM,
+} from "@swissgeo/coordinates";
 
 import {
   coordinatesUtils,
+  latLonToMGRS,
+  latLonToUTM,
   LV03,
   LV95,
   WEBMERCATOR,
@@ -10,10 +16,6 @@ import {
 import { formatThousand } from "@swissgeo/numbers";
 import { format as formatCoordinate, toStringHDMS } from "ol/coordinate";
 import proj4 from "proj4";
-
-import type { UTM } from "@/utils/militaryGridProjection";
-
-import { latLonToMGRS, latLonToUTM } from "@/utils/militaryGridProjection";
 
 type CoordinateFormatCallback = (
   coordinates: SingleCoordinate,
