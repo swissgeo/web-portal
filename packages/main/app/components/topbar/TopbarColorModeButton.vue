96f6c317957b6e2d375639008f11ee12b09320a0
Raphael Antonietti <raphael.antonietti@swisstopo.ch>
2026-08-26 17:26:23 +0200

    chore: restore dark mode toggle

---
+++ packages/main/app/components/topbar/TopbarColorModeButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+  <UColorModeButton
+    :label="t('topbar.modeSwitch')"
+    color="primary"
+    variant="ghost"
+    size="md"
+  />
+</template>
--- packages/main/nuxt.config.ts
+++ packages/main/nuxt.config.ts
@@ -55,7 +55,6 @@ export default defineNuxtConfig({
     experimental: {
       componentDetection: ["LocaleSelect"],
     },
-    colorMode: false,
   },
   sourcemap: {
     // Enable in dev, disable in prod (unless you have a private uploader)
