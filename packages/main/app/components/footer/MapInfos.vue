6bca8a2f1348ea6d80dccb5cb03266332f687c93
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-09-16 14:08:33 +0200

    GPS-1019: report issue form trigger from footer

--- packages/main/app/components/footer/MapInfos.vue
+++ packages/main/app/components/footer/MapInfos.vue
@@ -5,8 +5,10 @@ const { olMap } = storeToRefs(useMapStore());
 </script>
 
 <template>
-  <div class="flex w-fit flex-col items-center gap-2 sm:flex-row sm:gap-8">
-    <div class="w-60 justify-items-center">
+  <div
+    class="flex w-fit flex-col items-start gap-2 sm:flex-row sm:items-center sm:gap-8"
+  >
+    <div class="ml-2 w-60">
       <OLMapScale :olMap="olMap" class="footerMapScale" />
     </div>
     <OLMapMouseTracker />
