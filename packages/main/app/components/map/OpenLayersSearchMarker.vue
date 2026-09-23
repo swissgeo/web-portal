6d00f4ad04febe7d4bc7443e2402d92093b480be
Ismail Sunni <imajimatika@gmail.com>
2026-09-23 08:30:36 +0700

    GPS-1050: move the balloon pin into its own SVG file

--- packages/main/app/components/map/OpenLayersSearchMarker.vue
+++ packages/main/app/components/map/OpenLayersSearchMarker.vue
@@ -8,6 +8,7 @@ import type { Ref } from "vue";
 
 import { useAddLayerToMap } from "@swissgeo/map";
 import { useSearchStore } from "@swissgeo/skeleton";
+import balloonPinUrl from "~/assets/images/balloon_pin.svg";
 import Feature from "ol/Feature";
 import { Point } from "ol/geom";
 import { Vector as VectorLayer } from "ol/layer";
@@ -22,18 +23,13 @@ const coordinate = computed(() => searchStore.pinnedCoordinate);
 
 const olMap = inject<Ref<Map | undefined>>("olMap");
 
-const balloonPin = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="38" viewBox="0 0 26 38">
-  <path d="M13 2C7.5 2 3 6.5 3 12c0 6.5 10 24 10 24s10-17.5 10-24c0-5.5-4.5-10-10-10z" fill="#dc2626" stroke="#fff" stroke-width="2"/>
-  <circle cx="13" cy="12" r="4" fill="#fff"/>
-</svg>`;
-
 const pointFeature = new Feature({
   geometry: new Point(coordinate.value ?? [0, 0]),
 });
 pointFeature.setStyle(
   new Style({
     image: new Icon({
-      src: `data:image/svg+xml;utf8,${encodeURIComponent(balloonPin)}`,
+      src: balloonPinUrl,
       // the tip of the pin is what sits on the coordinate
       anchor: [0.5, 1],
     }),
